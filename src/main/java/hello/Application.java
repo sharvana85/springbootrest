3941d6df06dbcc74f428b290b08dfe2aa6c7564e
saravana <sharvana@gmail.com>
2019-08-10 12:40:09 +0530

    Added Sysout

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("Spring Boot Applicaiton Started ...");
         SpringApplication.run(Application.class, args);
     }
 }
